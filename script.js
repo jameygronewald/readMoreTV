c33cc0893297f16aec0f8c52b072861308c61757
Jamey Gronewald <jrgronewald@gmail.com>
2020-06-19 08:54:43 -0700

    got book data to display to search results screen; also wrapped code in document.ready function

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
-// INITIALIZING GLOBAL VARIABLES
-let genres = [];
+$(document).ready(function() {
+    // INITIALIZING GLOBAL VARIABLES
+    let genres = [];
 
-$('#searchButton').on('click', function(event) {
-    event.preventDefault();
-    // API call to tvmaze
-    let tvShow = $('#inputSearch').val();
-    let queryURL = 'http://api.tvmaze.com/singlesearch/shows?q=' + tvShow;
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).then(function(response){
-        console.log(response);
-        genres = [];
-        let i = 0;
-        while (i < response.genres.length) {
-            let showGenre = response.genres[i];
-            genres.push(showGenre);
-            i++;
-        };
-        console.log(genres);
-        $('.container').empty();
-        //API call to google books
-        let queryURL2 = 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyDQcHbPNLRpWvqCjR3cYCQgwCK3Llt09M0&q=subject:' + genres[0];
+    $('#searchButton').on('click', function(event) {
+        event.preventDefault();
+        // API call to tvmaze
+        let tvShow = $('#inputSearch').val();
+        let queryURL = 'http://api.tvmaze.com/singlesearch/shows?q=' + tvShow;
         $.ajax({
-            url: queryURL2,
+            url: queryURL,
             method: 'GET'
-        }).then(function(response) {
+        }).then(function(response){
             console.log(response);
+            genres = [];
+            let i = 0;
+            while (i < response.genres.length) {
+                let showGenre = response.genres[i];
+                genres.push(showGenre);
+                i++;
+            };
+            console.log(genres);
+            $('.container').empty();
+            //API call to google books
+            let queryURL2 = 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyDQcHbPNLRpWvqCjR3cYCQgwCK3Llt09M0&q=subject:' + genres[0];
+            $.ajax({
+                url: queryURL2,
+                method: 'GET'
+            }).then(function(response) {
+                console.log(response);
+                let bookAuthor = response.items[0].volumeInfo.authors[0];
+                let bookTitle = response.items[0].volumeInfo.title;
+                let bookYear = response.items[0].volumeInfo.publishedDate;
+                let bookDesc = response.items[0].volumeInfo.description;
+                console.log(bookAuthor, bookTitle, bookYear, bookDesc);
+
+
+            });
         });
-    });
 
-}); 
+    });   
+});
 
-
-// https://www.googleapis.com/books/v1/volumes?q=search+terms
-/* http://api.tvmaze.com/singlesearch/shows?q=Pok%C3%A9mon */
+    // https://www.googleapis.com/books/v1/volumes?q=search+terms
+    /* http://api.tvmaze.com/singlesearch/shows?q=Pok%C3%A9mon */
