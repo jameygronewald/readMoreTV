61e6b2d398a0452e36444772832ecd0e3f472e7e
Jamey Gronewald <jrgronewald@gmail.com>
2020-06-19 08:08:04 -0700

    added line to js to empty homepage elements from container; added ids to allow user input and search button to trigger API calls

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 // INITIALIZING GLOBAL VARIABLES
 let genres = [];
 
-$('#button').on('click', function(event) {
+$('#searchButton').on('click', function(event) {
     event.preventDefault();
     // API call to tvmaze
-    let queryURL = 'http://api.tvmaze.com/singlesearch/shows?q=Pok%C3%A9mon'
+    let tvShow = $('#inputSearch').val();
+    let queryURL = 'http://api.tvmaze.com/singlesearch/shows?q=' + tvShow;
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function(response){
         console.log(response);
+        genres = [];
         let i = 0;
         while (i < response.genres.length) {
             let showGenre = response.genres[i];
             genres.push(showGenre);
             i++;
-        }
+        };
         console.log(genres);
+        $('.container').empty();
+        //API call to google books
         let queryURL2 = 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyDQcHbPNLRpWvqCjR3cYCQgwCK3Llt09M0&q=subject:' + genres[0];
         $.ajax({
             url: queryURL2,
             method: 'GET'
         }).then(function(response) {
             console.log(response);
-        })
-    })
+        });
+    });
 
-    //API call to google books
 }); 
 
 
