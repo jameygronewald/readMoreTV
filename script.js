7f98e0f7bde43bd81370260a0ecb83c0bb103795
Jamey Gronewald <jrgronewald@gmail.com>
2020-06-20 23:10:16 -0700

    added a loop to generate totalGenres string that is a combination of all the genres of the given tv show; this string is then used in queryURL2 to search for more specific books

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
             method: 'GET'
         }).then(function(response){
             console.log(response);
+            console.log(response.summary);
             genres = [];
             let i = 0;
             while (i < response.genres.length) {
@@ -20,16 +21,29 @@ $(document).ready(function() {
                 i++;
             };
             console.log(genres);
+            let totalGenres = '';
+            let j = 0;
+            while (j < genres.length) {
+                if (j < genres.length - 1) {
+                    totalGenres += genres[j] + '+';
+                }
+                else {
+                    totalGenres += genres[j];
+                };
+                j++;
+                console.log(totalGenres);
+            };
             $('.container-fluid').empty();
             $('.container-books').empty();
             //API call to google books
-            let queryURL2 = 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyDQcHbPNLRpWvqCjR3cYCQgwCK3Llt09M0&langRestrict=en&q=subject:' + genres[0];
+            let queryURL2 = 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyDQcHbPNLRpWvqCjR3cYCQgwCK3Llt09M0&langRestrict=en&q=subject:' + totalGenres;
             $.ajax({
                 url: queryURL2,
                 method: 'GET'
             }).then(function(response) {
+                console.log(queryURL2);
                 console.log(response);
-                searchAgainRow = $('<div class="row"><div class ="col-sm-12"><h5 id="prompt">Didn\'t find what you were looking for?</h5></div></div>');
+                searchAgainRow = $('<div class="row"><div class ="col-sm-12"><h5 id="prompt">Try another search!</h5></div></div>');
                 $('.container-fluid').append(searchAgainRow);
                 console.log(tvShow);
                 let i = 0;
@@ -43,13 +57,11 @@ $(document).ready(function() {
                     let searchResultsRow = $('<div class="row"><div class="col-sm-1"><img id = "book' + i + 'Cover" src =' + bookCover + '></img></div><div class="col-sm-2 bookDescription"><p id = "book' + i + 'Title">' + bookTitle + '</p><p id = "book' + i + 'Author">' + bookAuthor + '</p><p id = "book' + i + 'Year">' + bookYear + '</p></div><div id = "book' + i + 'Desc" class="col-sm-8">' + bookDesc + '</div><div class="col-sm-1"><a href="' + bookPrice + '" class="btn btn-link active" role="button" target="_blank" aria-pressed="true">Buy Here</a></div></div><br>');
                     $(".container-books").append(searchResultsRow);
                     i++;
-
-                    $('#navTitle').on('click', function(){
-                        event.preventDefault();
-                        $('.container-books').empty();
-                        location.reload();
-                    })
-                }
+                };
+                $('#navTitle').on('click', function(){
+                    event.preventDefault();
+                    location.reload();
+                });
             });
         });
       
