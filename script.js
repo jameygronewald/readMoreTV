340c821fe8febd10a0cdc14b4436d3a5e79e0a50
Justin <ponthierjustin@gmail.com>
2020-06-21 13:19:24 -0400

    made small changes, but need to pull main code again

--- script.js
+++ script.js
@@ -1,57 +1,81 @@
-$(document).ready(function() {
-    // INITIALIZING GLOBAL VARIABLES
-    let genres = [];
+$(document).ready(function () {
+  // INITIALIZING GLOBAL VARIABLES
+  let genres = [];
 
-    $('#searchButton').on('click', function(event) {
-        event.preventDefault();
-        // API call to tvmaze
-        let tvShow = $('#inputSearch').val();
-        let queryURL = 'https://api.tvmaze.com/singlesearch/shows?q=' + tvShow;
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        }).then(function(response){
-            console.log(response);
-            genres = [];
-            let i = 0;
-            while (i < response.genres.length) {
-                let showGenre = response.genres[i];
-                genres.push(showGenre);
-                i++;
-            };
-            console.log(genres);
-            $('.container-fluid').empty();
-            $('.container-books').empty();
-            //API call to google books
-            let queryURL2 = 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyDQcHbPNLRpWvqCjR3cYCQgwCK3Llt09M0&q=subject:' + genres[2];
-            $.ajax({
-                url: queryURL2,
-                method: 'GET'
-            }).then(function(response) {
-                console.log(response);
-                let i = 0;
-                while (i < 10) {
-                    let bookAuthor = response.items[i].volumeInfo.authors[0];
-                    let bookTitle = response.items[i].volumeInfo.title;
-                    let bookYear = response.items[i].volumeInfo.publishedDate;
-                    let bookDesc = response.items[i].volumeInfo.description;
-                    let bookCover = response.items[i].volumeInfo.imageLinks.thumbnail;
-                    let bookPrice = response.items[i].saleInfo.buyLink;
-                    console.log(bookAuthor, bookTitle, bookYear, bookDesc, bookCover, bookPrice);
+  $("#searchButton").on("click", function (event) {
+    event.preventDefault();
+    // API call to tvmaze
+    let tvShow = $("#inputSearch").val();
+    let queryURL = "https://api.tvmaze.com/singlesearch/shows?q=" + tvShow;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      genres = [];
+      let i = 0;
+      while (i < response.genres.length) {
+        let showGenre = response.genres[i];
+        genres.push(showGenre);
+        i++;
+      }
+      console.log(genres);
+      $(".container-fluid").empty();
+      $(".container-books").empty();
+      //API call to google books
+      let queryURL2 =
+        "https://www.googleapis.com/books/v1/volumes?key=AIzaSyDQcHbPNLRpWvqCjR3cYCQgwCK3Llt09M0&orderBy=relevance&maxResults=40&q=subject" +
+        genres;
+      $.ajax({
+        url: queryURL2,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
+        let i = 0;
+        while (i < 40) {
+          
+          let bookAuthor = response.items[i].volumeInfo.authors;
+          let bookTitle = response.items[i].volumeInfo.title;
+          let bookYear = response.items[i].volumeInfo.publishedDate;
+          let bookDesc = response.items[i].volumeInfo.description;
+          let bookCover = response.items[i].volumeInfo.imageLinks.thumbnail;
+          let bookPrice = response.items[i].saleInfo.buyLink;
+          console.log(
+            bookAuthor,
+            bookTitle,
+            bookYear,
+            bookDesc,
+            bookCover,
+            bookPrice
+          );
 
-                    let searchResultsRow = $('<div class="row"><div class="col-sm-1"><img id = "book1Cover" src =' + bookCover + '></img></div><div class="col-sm-2"><p id = "book1Title">' + bookTitle + '</p><p id = "book1Author">' + bookAuthor + '</p><p id = "book1Year">' + bookYear + '</p></div><div id = "book1Desc" class="col-sm-8">' + bookDesc + '</div><div class="col-sm-1"><a href="' + bookPrice + '" class="btn btn-link active" role="button" target="_blank" aria-pressed="true">Buy Here</a></div></div><br>');
-                    $(".container-books").append(searchResultsRow);
-                    i++;
+          let searchResultsRow = $(
+            '<div class="row"><div class="col-sm-1"><img id = "book1Cover" src =' +
+              bookCover +
+              '></img></div><div class="col-sm-2"><p id = "book1Title">' +
+              bookTitle +
+              '</p><p id = "book1Author">' +
+              bookAuthor +
+              '</p><p id = "book1Year">' +
+              bookYear +
+              '</p></div><div id = "book1Desc" class="col-sm-8">' +
+              bookDesc +
+              '</div><div class="col-sm-1"><a href="' +
+              bookPrice +
+              '" class="btn btn-link active" role="button" target="_blank" aria-pressed="true">Buy Here</a></div></div><br>'
+          );
+          $(".container-books").append(searchResultsRow);
+          i++;
 
-                    $('#navTitle').on('click', function(){
-                        event.preventDefault();
-                        $('.container-books').empty();
-                        location.reload();
-                    })
-                }
-            });
-        });
-    });   
+          $("#navTitle").on("click", function () {
+            event.preventDefault();
+            $(".container-books").empty();
+            location.reload();
+          });
+        }
+      });
+    });
+  });
 });
-    // https://www.googleapis.com/books/v1/volumes?q=search+terms
-    /* http://api.tvmaze.com/singlesearch/shows?q=Pok%C3%A9mon */
+// https://www.googleapis.com/books/v1/volumes?q=search+terms
+/* http://api.tvmaze.com/singlesearch/shows?q=Pok%C3%A9mon */
