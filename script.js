f3f3334b3a773ade78449d25efd4035f57545196
Justin <ponthierjustin@gmail.com>
2020-06-21 22:40:22 -0400

    adjusted button placements

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
             $('.container-fluid').empty();
             $('.container-books').empty();
             //API call to google books
-            let queryURL2 = 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyDQcHbPNLRpWvqCjR3cYCQgwCK3Llt09M0&langRestrict=en&maxResults=40&q=subject:' + genres[0];
+            let queryURL2 = 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyDQcHbPNLRpWvqCjR3cYCQgwCK3Llt09M0&langRestrict=en&maxResults=40&q=subject:' +  genres[0];
             $.ajax({
                 url: queryURL2,
                 method: 'GET'
@@ -41,7 +41,7 @@ $(document).ready(function () {
                     let bookDesc = response.items[i].volumeInfo.description;
                     let bookCover = response.items[i].volumeInfo.imageLinks.thumbnail;
                     let bookPrice = response.items[i].saleInfo.buyLink;
-                    let searchResultsRow = $('<div class="row"><div class="col-lg-2"><img id = "book' + i + 'Cover" src =' + bookCover + '></img></div><div class="col-md-2 bookDescription"><p id = "book' + i + 'Title">' + "Title: "+ bookTitle + '</p><p id = "book' + i + 'Author">' + "Author: " + bookAuthor + '</p><p id = "book' + i + 'Year">' + "Release Date: " + bookYear + '</p></div><div id = "book' + i + 'Desc" class="col-sm-8">' + bookDesc + '</div><div class="col-sm-1"><a href="' + bookPrice + '" class="btn btn-link active" role="button" target="_blank" aria-pressed="true">Buy Here</a></div></div><br>');
+                    let searchResultsRow = $('<div class="row border"><div class="col-lg-2"><img id = "book' + i + 'Cover" src =' + bookCover + '></img></div><div class="col-md-2 bookDescription"><p id = "book' + i + 'Title">' + "Title: "+ bookTitle + '</p><p id = "book' + i + 'Author">' + "Author: " + bookAuthor + '</p><p id = "book' + i + 'Year">' + "Release Date: " + bookYear + '</p><a href="' + bookPrice + '" class="btn btn-dark active rounded-pill" role="button" target="_blank" aria-pressed="true">Buy Here</a></div><div id = "book' + i + 'Desc" class="col-sm-8">' + bookDesc + '</div><div class="col-sm-1"></div></div><br>');
                     $(".container-books").append(searchResultsRow);
                     i++;
 
